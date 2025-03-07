d9317b55531f9132716bde865b5550f3187c8ace
bartoszOlewinski <bartosz.olewinski@gmail.com>
2025-03-07 17:40:36 +0100

    Added extra entity package and more classes

--- src/main/java/controller/RepoController.java
+++ src/main/java/controller/RepoController.java
@@ -5,8 +5,10 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jdk.jfr.Registered;
 import org.jboss.resteasy.reactive.RestPath;
 
+@Registered
 @Path("https://api.github.com")
 public class RepoController {
 
@@ -14,7 +16,7 @@ public class RepoController {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getRepositoryList(@RestPath String username) {
-        return "Hello from Quarkus REST";
+        return "accessing repos for user " + username;
     }
 
     @Path("/repos/{owner}/{repo}/branches")
